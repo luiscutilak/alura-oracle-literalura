396170a7a39c87e2284e28c0bd7ec261acfad876
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-07-04 23:58:50 -0300

    feat: Implementando interação com o usuário.

--- src/main/java/br/com/alura/literalura/principal/Principal.java
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -1,4 +1,18 @@
 package br.com.alura.literalura.principal;
 
+import br.com.alura.literalura.model.DadosLivro;
+import br.com.alura.literalura.service.ConsumoApi;
+import br.com.alura.literalura.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Principal {
+    private Scanner sc = new Scanner(System.in);
+    private ConsumoApi csm = new ConsumoApi();
+    private ConverteDados cnd = new ConverteDados();
+
+    private final String ENDERECO = "https://gutendex.com/books?search=";
+    private List<DadosLivro> dadosLivros = new ArrayList<>();
 }
